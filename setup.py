83af575ebdc96f32d7a261d6f50ef9e04787a60f
David Sparrow <dsparrow27@gmail.com>
2018-04-16 18:53:39 +1000

    plugin manager update

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="slither",
+    version="0.1.0",
+    description="Execution graph",
+    author="David Sparrow",
+    author_email="dsparrow27@gmail.com",
+    url="https://github.com/dsparrow27/slither",
+    license="GNU",
+    install_requires=["blinker"],
+    dependency_links=["https://github.com/dsparrow27/zoocore"],
+    packages=find_packages(),
+    zip_safe=False,
+)
